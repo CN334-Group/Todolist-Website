ed57a1afe59a8f4a442f1a0c36db13229469876d
monosik <tot74800@gmail.com>
2022-03-23 11:29:40 +0700

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
 class Task extends Model
-{    
+{   
+    protected $fillable = [
+        'user_id',
+        'description',
+    ];
+    
     public function user()
     {
     	return $this->belongsTo(User::class);
