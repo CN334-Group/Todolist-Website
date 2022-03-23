76eb4aa281ef17bb396ac4a2d273d518ee2cdffd
Osawa.ryu <60128482+K656l@users.noreply.github.com>
2022-03-23 11:47:27 +0700

    seeder test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        $this->call(
+            UserSeeder::class
+        );
     }
 }
