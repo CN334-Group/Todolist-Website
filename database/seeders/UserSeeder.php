8d5b9652f14e74cae3d0f48a2e81cf8e8340fa37
Osawa.ryu <60128482+K656l@users.noreply.github.com>
2022-04-20 19:22:46 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,19 +5,19 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class UserSeeder extends Seeder
-{
-    // /**
-    //  * Run the database seeders.
-    //  *
-    //  * @return void
-    //  */
-    // public function run()
-    // {
-    //     User::Table('users')->insert([
-    //         'name' => Str::random(10),
-    //         'email' => Str::random(10).'@gmail.com',
-    //         'password' => Hash::make('password'),
-    //     ]);
-    // }
-}
+// class UserSeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeders.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+//         User::Table('users')->insert([
+//             'name' => Str::random(10),
+//             'email' => Str::random(10).'@gmail.com',
+//             'password' => Hash::make('password'),
+//         ]);
+//     }
+// }
