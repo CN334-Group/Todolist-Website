a4ed493b7697f005a5316d74f17a2ca1cee53987
monosik <tot74800@gmail.com>
2022-04-20 10:33:37 +0700

    Update UserUnitTest.php

--- tests/Unit/UserUnitTest.php
+++ tests/Unit/UserUnitTest.php
@@ -26,4 +26,26 @@ class UserUnitTest extends TestCase
         $this->assertEquals('Test User', $user->name);
     }
 
+    public function Test_User_Thai_Name_Creation()
+    {
+        $user = new User([
+            'name' => "ยูสเซอร์",
+            'email' => "test@mail.com",
+            'password' => bcrypt("testpassword")
+        ]);   
+
+        $this->assertEquals('Test User', $user->name);
+    }
+
+    public function Test_User_Thai_Password_Creation()
+    {
+        $user = new User([
+            'name' => "Test User",
+            'email' => "test@mail.com",
+            'password' => bcrypt("พาสเวิร์ด")
+        ]);   
+
+        $this->assertEquals('Test User', $user->name);
+    }
+
 }
